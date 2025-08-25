fe1fdef9274ce5bdee48feeb7683c43d1926d36e
BND <benjamin.dalemans@gmail.com>
2025-08-25 15:45:07 +0200

    Decomposition of the template into partials.

--- app/views/templates/partials/_main.php
+++ app/views/templates/partials/_main.php
@@ -1,4 +1,11 @@
-<?php 
+ <section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 ftco-animate">
+            
+          <?php echo $content ?>
 
-    echo $content;
-    include '../app/views/templates/partials/_sidebar.php';
+        </div>
+        <?php include '../app/views/templates/partials/_aside.php'; ?>
+      </div>
+    </section> <!-- .section -->
